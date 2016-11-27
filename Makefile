a90100b5f69537d3ca392cf38b9e708f97fa315f
Hiraku NAKANO <hiraku@tojiru.net>
2016-11-27 10:41:53 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,76 @@
+.DEFAULT_GOAL := help
+version := 7.0.12
+
+help: ## このヘルプを表示する
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+	@echo make clean version=7.0.0
+	@echo make install version=7.0.0
+	@echo make switch version=7.0.0
+
+.PHONY: current
+current: ## 現在のphp version
+	@php -v
+
+.PHONY: ls
+ls: ## マシンにインストールされているphpの一覧を取得します version不要
+	@ls ~/.php/
+
+.PHONY: ls-remote
+ls-remote: ## インストールできそうなstable版phpの一覧を表示します version不要
+	@php phpversions.php
+
+.PHONY: switch
+switch: ## マシンのデフォルトphpをversionに変更します
+	@(cd ~/.php && rm current && ln -s $(version) current)
+	@php -v
+
+.PHONY: clean
+clean: ## 指定されたバージョンのディレクトリをmake cleanします
+	(cd php-$(version) && make clean)
+
+.PHONY: fullclean
+fullclean: ## 指定されたバージョンの関連ファイルを完全削除します
+	rm -rf php-$(version)
+	rm -rf php-$(version).tar.bz2
+
+.PHONY: uninstall
+uninstall: ## 指定されたバージョンを~/.phpから削除します
+	rm -rf ~/.php/$(version)
+
+# ~~~~~~~~
+php-$(version).tar.bz2:
+	curl -Lo php-$(version).tar.bz2 "http://jp2.php.net/get/php-$(version).tar.bz2/from/this/mirror"
+
+php-$(version): php-$(version).tar.bz2
+	tar xf php-$(version).tar.bz2
+
+php-$(version)/sapi/cli/php: php-$(version)
+	(cd php-$(version) && \
+	./configure \
+		--prefix=$(HOME)/.php/$(version) \
+		--enable-dtrace \
+		--enable-phpdbg \
+		--enable-phpdbg-webhelper \
+		--enable-pcntl \
+		--enable-bcmath \
+		--enable-calendar \
+		--enable-exif \
+		--enable-sockets \
+		--enable-intl \
+		--with-icu-dir=/usr/local/opt/icu4c \
+		--with-openssl=/usr/local/opt/openssl \
+		--with-curl=/usr/local/opt/curl \
+		--with-readline=/usr/local/opt/readline \
+		--with-libxml-dir=/usr/local/opt/libxml2 \
+		--with-bz2 \
+		--enable-mbstring \
+		--enable-mysqlnd \
+		--enable-zip \
+		--enable-re2c-cgoto && \
+	make -j2 )
+
+~/.php/$(version): php-$(version)/sapi/cli/php
+	(cd php-$(version) && make install)
+
+.PHONY: install
+install: ~/.php/$(version)
---
+++ phpversions.php
@@ -0,0 +1,87 @@
+<?php
+
+function main($argc, array $argv)
+{
+    $host = getenv('PHP_NET_HOST');
+    if (!$host) {
+        $host = 'http://jp2.php.net';
+    }
+
+    $phpall = download($host . '/releases/index.php?serialize');
+    $phpall = unserialize($phpall);
+    foreach ($phpall as $major => $info) {
+        $phpver = download("$host/releases/index.php?serialize&version=$major&max=1000");
+        $phpver = unserialize($phpver);
+        $phpver = array_keys($phpver);
+
+        say("\n## $major");
+        usort($phpver, 'version_compare');
+
+        $miner = 0;
+        $i = 0;
+        $basever = "$major.$miner";
+        say("### $basever");
+        foreach ($phpver as $ver) {
+            if (0 !== strncmp($ver, $basever, strlen($basever))) {
+                ++$miner;
+                $basever = "$major.$miner";
+                say("\n\n### $basever");
+                $i = 0;
+            }
+            echo "$ver\t";
+            if (++$i > 9) {
+                echo PHP_EOL;
+                $i = 0;
+            }
+        }
+
+        echo PHP_EOL;
+    }
+
+    return 0;
+}
+
+function say(string $str)
+{
+    echo $str, PHP_EOL;
+}
+
+interface CurlException
+{
+}
+
+class ConnectException extends RuntimeException implements CurlException
+{
+}
+
+class HttpException extends RuntimeException implements CurlException
+{
+}
+
+function download($url) {
+    static $ch;
+    if (!$ch) $ch = curl_init();
+
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => '',
+    ]);
+
+    $response = curl_exec($ch);
+
+    $errno = curl_errno($ch);
+    if ($errno !== CURLE_OK) {
+        throw new ConnectException(curl_error($ch), $errno);
+    }
+
+    $info = curl_getinfo($ch);
+    if ($info['http_code'] >= 400) {
+        throw new HttpException($info['http_code'], $response);
+    }
+
+    return $response;
+}
+
+die(main($argc, $argv));
